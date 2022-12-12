d08d67a74bdd8010f2c6a1b737845b1feed09952
nitinnavik <naviknitin143@gmail.com>
2022-12-12 08:50:54 +0530

    changes.

---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+import useSWR from "swr";
+
+export function Profile() {
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR("/api/hello", fetcher);
+  console.log(data);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return <div>hello {data.name}!</div>;
+}
